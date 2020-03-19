657e20c4004bb4ade45007dc5c8a85254fd2cc6c
Chen Osipov <eobender@gmail.com>
2020-03-19 16:36:55 -0700

    fixes

--- src/config.js
+++ src/config.js
@@ -1,36 +1,33 @@
-const readPkgUp = require('read-pkg-up')
+const readPkgUp = require("read-pkg-up");
 
-const pkg = readPkgUp.sync().pkg
-const program = require('commander')
-const { error } = require('prettycli')
-const debug = require('./debug')
+const pkg = readPkgUp.sync().pkg;
+const program = require("commander");
+const { error } = require("prettycli");
+const debug = require("./debug");
 
 /* Config from package.json */
-const packageJSONconfig = pkg.bundlesize
+const packageJSONconfig = pkg["bundle-range"];
 
 /* Config from CLI */
 
 program
-  .option('-f, --files [files]', 'files to test against (dist/*.js)')
-  .option('-s, --max-size [maxSize]', 'maximum size threshold (3Kb)')
-  .option('--min-size [minSize]', 'minimum size threshold (2Kb)')
-  .option('--debug', 'run in debug mode')
-  .option(
-    '-c, --compression [compression]',
-    'specify which compression algorithm to use'
-  )
-  .parse(process.argv)
+  .option("-f, --files [files]", "files to test against (dist/*.js)")
+  .option("-s, --max-size [maxSize]", "maximum size threshold (3Kb)")
+  .option("--min-size [minSize]", "minimum size threshold (2Kb)")
+  .option("--debug", "run in debug mode")
+  .option("-c, --compression [compression]", "specify which compression algorithm to use")
+  .parse(process.argv);
 
-let cliConfig
+let cliConfig;
 if (program.files) {
   cliConfig = [
     {
       path: program.files,
       maxSize: program.maxSize,
       minSize: program.minSize,
-      compression: program.compression || 'gzip'
+      compression: program.compression || "gzip"
     }
-  ]
+  ];
 }
 
 /* Send to readme if no configuration is provided */
@@ -43,13 +40,13 @@ if (!packageJSONconfig && !cliConfig) {
     https://github.com/siddharthkp/bundlesize#configuration
   `,
     { silent: true }
-  )
+  );
 }
 
-const config = cliConfig || packageJSONconfig
+const config = cliConfig || packageJSONconfig;
 
-debug('cli config', cliConfig)
-debug('package json config', packageJSONconfig)
-debug('selected config', config)
+debug("cli config", cliConfig);
+debug("package json config", packageJSONconfig);
+debug("selected config", config);
 
-module.exports = config
+module.exports = config;
